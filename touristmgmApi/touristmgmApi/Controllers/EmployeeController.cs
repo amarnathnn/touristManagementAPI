f4fac0393879a9de8ed06aa18612fa5618680a1b
amarnathnn <amarnathnn@gmail.com>
2022-11-30 18:54:28 +0530

    test email

--- touristmgmApi/touristmgmApi/Controllers/EmployeeController.cs
+++ touristmgmApi/touristmgmApi/Controllers/EmployeeController.cs
@@ -25,7 +25,7 @@ namespace touristmgmApi.Controllers
             _logger = logger;
             Profiles = new List<AddBranchModel>();
             Profiles.Add(new AddBranchModel() { BranchName = "Andaman",  Contact = "9923412345", EmailId = "amarnathnn@gmail.com" });
-            Profiles.Add(new AddBranchModel() { BranchName = "Singapore", Contact = "9923412345", EmailId = "fmamil@gmail.com" });
+            Profiles.Add(new AddBranchModel() { BranchName = "Singapore", Contact = "9923412345", EmailId = "test@gmail.com" });
         }
 
 
